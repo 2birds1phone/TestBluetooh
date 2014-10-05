8bfae20b524fb19bd35d0689a217a60877534db7
2birds1phone <2birds1phone@gmail.com>
2014-10-05 16:10:08 +0200

    1st commit

---
+++ BluetoothSynchroTest/src/com/example/bluetoothsynchrotest/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.bluetoothsynchrotest;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.os.Build;
+import android.bluetooth.*;
+
+import java.util.*;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+    	super.onStart();
+    	TextView text = (TextView) this.findViewById(R.id.textview);
+    	text.setText("Test");
+    	
+    	BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> setdevices = adapter.getBondedDevices();
+        
+        if(adapter.startDiscovery())
+        	text.append("\r\nDiscovery ok ");
+        
+        Iterator<BluetoothDevice> it = setdevices.iterator();
+        while(it.hasNext()) 
+        	text.append(setdevices.toString()+ "  " + it.next().getName() );
+    }
+    
+    protected void client() {
+    	
+    	
+    }
+    
+    protected void server() {
+    	
+    }
+    
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+}
