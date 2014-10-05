3863ea2c279ea804c7ccfcf09b800f6f1fdd6050
2birds1phone <2birds1phone@gmail.com>
2014-10-05 19:48:48 +0200

    xaxaxa

--- BluetoothSynchroTest/src/com/example/bluetoothsynchrotest/MainActivity.java
+++ BluetoothSynchroTest/src/com/example/bluetoothsynchrotest/MainActivity.java
@@ -18,101 +18,127 @@ import java.util.*;
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	// Create a BroadcastReceiver for ACTION_FOUND
+	private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+	    public void onReceive(Context context, Intent intent) {
+	        String action = intent.getAction();
+	        // When discovery finds a device
+	        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+	            // Get the BluetoothDevice object from the Intent
+	            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+	            // Add the name and address to an array adapter to show in a ListView
+	            //text.add(device.getName() + "\n" + device.getAddress());
+	        }
+	    }
+	};
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+		
 
-    @Override
-    protected void onStart() {
-    	super.onStart();
-    	TextView text = (TextView) this.findViewById(R.id.textview);
-    	text.setText("Test");
-    	
-    	BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
-    	if (adapter == null) {
-    		//Bluetooth not supported on this device
-    	}
-    	
-    	if (!adapter.isEnabled()) {
-    	    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-    	    startActivityForResult(enableBtIntent, 1);
-    	}
-    	
-        Set<BluetoothDevice> setdevices = adapter.getBondedDevices();
-        
-        if(adapter.startDiscovery())
-        	text.append("\r\nDiscovery ok ");
-        
-        Iterator<BluetoothDevice> it = setdevices.iterator();
-        while(it.hasNext()) 
-        	text.append(setdevices.toString()+ "  " + it.next().getName() );
-    }
-    
-    @Override
-    protected void onStop() {
-    	super.onStop();
-    	
-    	
-    	//turn off the bluetooth if opened by the app
-    	
-    	BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
-    	if (adapter == null) {
-    		//Bluetooth not supported on this device
-    	}
-    }
-    
-    protected void client() {
-    	
-    	
-    }
-    
-    protected void server() {
-    	
-    }
-    
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		
+		// Register the BroadcastReceiver
+		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+		registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	protected void onStart() {
+		super.onStart();
+		TextView text = (TextView) this.findViewById(R.id.textview);
+		text.setText("Test");
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+	}
 
-        public PlaceholderFragment() {
-        }
+	@Override
+	protected void onResume() {
+		super.onResume();
+		TextView text = (TextView) this.findViewById(R.id.textview);
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
+		BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+		if (adapter == null) {
+			// Bluetooth not supported on this device
+		}
+
+		Set<BluetoothDevice> pairedDevices = adapter.getBondedDevices();
+
+		if (!adapter.isEnabled()) {
+			Intent enableBtIntent = new Intent(
+					BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableBtIntent, 1);
+			// TODO if the user deny the access to the bluetooth
+		}
+
+		if (adapter.startDiscovery())
+			text.append("\r\nDiscovery ok ");
+
+		if (pairedDevices.size() > 0)
+			for (BluetoothDevice device : pairedDevices)
+				text.append(device.getName() + "  " + device.getAddress() + "\n");
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+
+		// TODO turn off the bluetooth if opened by the app
+
+		BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+		if (adapter == null) {
+			// Bluetooth not supported on this device
+		}
+	}
+
+	protected void client() {
+
+	}
+
+	protected void server() {
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
 
 }
